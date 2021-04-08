bd5d1dceda3bebc37c9fc100ab165ffcbf444760
Trevrosa <szehawong@gmail.com>
2021-04-08 17:22:39 +0800

    fixes to clipboard copying

--- LinkvertiseBypass/Program.cs
+++ LinkvertiseBypass/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Globalization;
 using System.IO;
 using System.Net;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Text.Json;
+using Xamarin.Essentials;
+using System.Globalization;
+using System.Threading.Tasks;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
@@ -27,12 +26,12 @@ namespace LinkvertiseBypass
         private static async Task MainAsync()
         {
             Console.Title = "Linkvertise Bypass";
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.Write("Write Url: ");
             Console.ForegroundColor = ConsoleColor.DarkGray;
 
-            API result = await BypassAsync(Console.ReadLine(), true);
+            API result = await BypassAsync(Console.ReadLine().Trim(), true);
             Console.ForegroundColor = ConsoleColor.Green;
 
             if (result.Url.Contains("invalid") | result is null)
@@ -48,7 +47,7 @@ namespace LinkvertiseBypass
                     link = link.Replace("pastebin.com", "pastebinp.com");
                 }
 
-                ClipboardSetText(link);
+                await Clipboard.SetTextAsync(link);
 
                 Console.WriteLine($"Input Url: {result.InputUrl}");
                 Console.WriteLine($"Url: {link}");
@@ -58,21 +57,98 @@ namespace LinkvertiseBypass
             await Task.Delay(-1);
         }
 
+        public static async Task<API> BypassAsync(string url, bool text)
+        {
+            if (text is true)
+            {
+                bool complete = false;
+                string status = "(Initialized! 1/3)              ";
+                API apiResult = null;
+
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                await Task.Delay(1000);
+
+                _ = Task.Run(async () =>
+                {
+                    Random Random = new Random();
+
+                    string resultUrl = LinkRegex.Replace(url, string.Empty);
+
+                    string randomProxy = Random.Next(11111111, 99999999).ToString();
+
+                    status = "(Link parsed! 2/3)                        ";
+
+                    await Task.Delay(1000);
+
+                    try
+                    {
+                        status = "(Getting link! 3/3)                          ";
+
+                        string value = await GetAsync($"https://beautiful-code.glasstea.repl.co/{resultUrl}/{randomProxy}");
+                        apiResult = JsonSerializer.Deserialize<API>(value);
+                        complete = true;
+                    }
+                    catch
+                    {
+                        complete = true;
+                    }
+                });
+
+                while (complete is false)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write($"\rBypassing. {status}");
+                    Console.SetCursorPosition(0, 0);
+
+                    await Task.Delay(100);
+
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write($"\rBypassing.. {status}");
+                    Console.SetCursorPosition(0, 0);
+
+                    await Task.Delay(100);
+
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write($"\rBypassing... {status}");
+                    Console.SetCursorPosition(0, 0);
+
+                    await Task.Delay(100);
+                }
+
+                Console.Clear();
+                return apiResult;
+            }
+            else
+            {
+                Random Random = new Random();
+
+                string resultUrl = LinkRegex.Replace(url, string.Empty);
+
+                string randomProxy = string.Format($"{0}{1}{2}{3}", new int[]
+                {
+                    Random.Next(1, 99),
+                    Random.Next(0, 99),
+                    Random.Next(0, 99),
+                    Random.Next(0, 99)
+                });
+
+                try
+                {
+                    string value = await GetAsync($"https://beautiful-code.glasstea.repl.co/{resultUrl}/{randomProxy}");
+                    API apiResult = JsonSerializer.Deserialize<API>(value);
+
+                    return apiResult;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
         #region Methods
-
-        private static void ClipboardSetText(string text)
-        {
-            Thread clipboardThread = new Thread(() => ClipboardThreadWorker(text));
-            clipboardThread.SetApartmentState(ApartmentState.STA);
-            clipboardThread.IsBackground = false;
-
-            clipboardThread.Start();
-        }
-
-        private static void ClipboardThreadWorker(string text)
-        {
-            Clipboard.SetText(text);
-        }
 
         public static string Remove(this string remove, string replace)
         {
@@ -99,83 +175,6 @@ namespace LinkvertiseBypass
         }
 
         #endregion Methods
-
-        public static async Task<API> BypassAsync(string url, bool text)
-        {
-            if (text is true)
-            {
-                bool complete = false;
-                API apiResult = null;
-
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Cyan;
-
-                _ = Task.Run(async () =>
-                {
-                    Random Random = new Random();
-
-                    string resultUrl = LinkRegex.Replace(url, string.Empty);
-
-                    string randomProxy = string.Format($"{0}{1}{2}{3}", new int[]
-                    {
-                        Random.Next(1, 99),
-                        Random.Next(0, 99),
-                        Random.Next(0, 99),
-                        Random.Next(0, 99)
-                    });
-
-                    try
-                    {
-                        string value = await GetAsync($"https://beautiful-code.glasstea.repl.co/{resultUrl}/{randomProxy}");
-                        apiResult = JsonSerializer.Deserialize<API>(value);
-                        complete = true;
-                    }
-                    catch
-                    {
-                        complete = true;
-                    }
-                });
-
-                while (complete is false)
-                {
-                    Console.Write("\rBypassing.");
-                    await Task.Delay(100);
-                    Console.Write("\rBypassing..");
-                    await Task.Delay(100);
-                    Console.Write("\rBypassing...");
-                    await Task.Delay(100);
-                }
-
-                Console.Clear();
-                return apiResult;
-            }
-            else
-            {
-                Random Random = new Random();
-
-                string resultUrl = LinkRegex.Replace(url, string.Empty);
-
-                string randomProxy = string.Format($"{0}{1}{2}{3}", new int[]
-                {
-                Random.Next(1, 99),
-                Random.Next(0, 99),
-                Random.Next(0, 99),
-                Random.Next(0, 99)
-                });
-
-                try
-                {
-                    string value = await GetAsync($"https://beautiful-code.glasstea.repl.co/{resultUrl}/{randomProxy}");
-                    API apiResult = JsonSerializer.Deserialize<API>(value);
-
-                    return apiResult;
-                }
-                catch
-                {
-                    return null;
-                }
-            }
-        }
 
         public class API
         {
